3861fd8fd53ad7fb8847e2763c58621b2b1ea701
Eric Li <eric.li.8736@gmail.com>
2024-06-12 14:02:24 -0500

    pass stage

--- app/main.c
+++ app/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-  // You can use print statements as follows for debugging, they'll be visible when running tests
-  // printf("Logs from your program will appear here!\n");
+  // You can use print statements as follows for debugging, they'll be visible
+  // when running tests printf("Logs from your program will appear here!\n");
 
   // Uncomment this block to pass the first stage
   printf("$ ");
@@ -11,5 +12,8 @@ int main() {
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
+
+  input[strlen(input) - 1] = '\0';
+  printf("%s: command not found\n", input);
   return 0;
 }
