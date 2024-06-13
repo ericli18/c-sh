60e8c2cba63bacca91c69b1a2a87a8b294867b61
Eric Li <eric.li.8736@gmail.com>
2024-06-13 15:31:56 -0500

    pass stage

--- app/main.c
+++ app/main.c
@@ -16,6 +16,9 @@ int main() {
     char input[100];
     fgets(input, 100, stdin);
     input[strlen(input) - 1] = '\0';
+    if (strcmp(input, "exit 0") == 0) {
+      return 0;
+    }
     printf("%s: command not found\n", input);
   }
   return 0;
