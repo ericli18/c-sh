6366cc9d48a5637c64d5553eb7d55f3801af5a35
Eric Li <eric.li.8736@gmail.com>
2024-06-13 08:58:08 -0500

    pass stage

--- app/main.c
+++ app/main.c
@@ -10,10 +10,13 @@ int main() {
   fflush(stdout);
 
   // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
 
-  input[strlen(input) - 1] = '\0';
-  printf("%s: command not found\n", input);
+  int yes = 0;
+  while (!yes) {
+    char input[100];
+    fgets(input, 100, stdin);
+    input[strlen(input) - 1] = '\0';
+    printf("%s: command not found\n", input);
+  }
   return 0;
 }
