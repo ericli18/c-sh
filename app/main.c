5e8cb05f86aa87b2a9a367b0bf6f77e3f6d6b8b5
Eric Li <eric.li.8736@gmail.com>
2024-06-13 16:22:08 -0500

    pass stage

--- app/main.c
+++ app/main.c
@@ -16,11 +16,26 @@ int main() {
     char input[100];
     fgets(input, 100, stdin);
     input[strlen(input) - 1] = '\0';
+    char *commands[] = {"exit", "type", "echo"};
+    size_t commands_len = 3;
     if (strcmp(input, "exit 0") == 0) {
       return 0;
-    }
-    if (strncmp("echo", input, sizeof("echo") - 1) == 0) {
-      printf("%s\n", input + 5);
+    } else if (strncmp("echo", input, strlen("echo")) == 0) {
+      printf("%s\n", input + strlen("echo "));
+    } else if (strncmp("type", input, strlen("type")) == 0) {
+      char *arg = input + strlen("type ");
+      int flag = 0;
+      for (int i = 0; i < commands_len; i++) {
+        if (strcmp(arg, commands[i]) == 0) {
+          printf("%s is a shell builtin\n", arg);
+          flag = 1;
+          break;
+        }
+      }
+      if (!flag) {
+        printf("%s: not found\n", arg);
+      }
+
     } else {
       printf("%s: command not found\n", input);
     }
