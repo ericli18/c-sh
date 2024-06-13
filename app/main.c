4f860c57c235e194322da079c8a177b863487f70
Eric Li <eric.li.8736@gmail.com>
2024-06-13 16:47:30 -0500

    pass stage

--- app/main.c
+++ app/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main() {
@@ -8,16 +9,21 @@ int main() {
   // Uncomment this block to pass the first stage
 
   // Wait for user input
+  char *const_path = getenv("PATH");
+  char *commands[] = {"exit", "type", "echo"};
+  size_t commands_len = 3;
 
   int yes = 0;
   while (!yes) {
+    char path[strlen(const_path) + 1];
+    strcpy(path, const_path);
+
     printf("$ ");
     fflush(stdout);
     char input[100];
     fgets(input, 100, stdin);
     input[strlen(input) - 1] = '\0';
-    char *commands[] = {"exit", "type", "echo"};
-    size_t commands_len = 3;
+
     if (strcmp(input, "exit 0") == 0) {
       return 0;
     } else if (strncmp("echo", input, strlen("echo")) == 0) {
@@ -32,6 +38,17 @@ int main() {
           break;
         }
       }
+      char *curr_path = strtok(path, ":");
+      while (curr_path != NULL) {
+        char *command = curr_path + (strlen(curr_path) - strlen(arg));
+        if (strcmp(command, arg) == 0) {
+          printf("%s is %s\n", arg, curr_path);
+          flag = 1;
+          break;
+        }
+        curr_path = strtok(NULL, ":");
+      }
+
       if (!flag) {
         printf("%s: not found\n", arg);
       }
